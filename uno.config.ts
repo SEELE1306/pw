bdbf91bcf7f0521350832205dba337f309f39c25
SEELE1306 <mkdang@proton.me>
2025-08-08 21:58:11 +0200

    Update uno config

--- uno.config.ts
+++ uno.config.ts
@@ -16,4 +16,42 @@ export default defineConfig({
             },
         }),
     ],
+
+    theme: {
+        colors: {
+            accent: '#a57562',
+            normal: '#a3a5aa',
+            vibrant: '#b5bac1',
+            brand: {
+                primary: '#262626',
+                secondary: '#232323',
+                tertiary: '#212121',
+                quarternary: '#282828',
+                slate: '#191919',
+            },
+        },
+        fontSize: {
+            sm: '0.875rem',
+            base: '1rem',
+            lg: '1.125rem',
+            xl: '1.25rem',
+        },
+    },
+
+    rules: [
+        [/^m-(\d+)$/, ([, d]) => ({ margin: `${d / 4}rem` })],
+        [/^p-(\d+)$/, match => ({ padding: `${match[1] / 4}rem` })],
+
+        ['bold', { 'font-weight': 700 }],
+        ['regular', { 'font-weight': 500 }],
+        ['light', { 'font-weight': 300 }],
+
+        ['center', { 'text-align': 'center' }],
+        ['right', { 'text-align': 'right' }],
+        ['left', { 'text-align': 'left' }],
+
+        ['sans', { 'font-family': 'Inter, sans-serif' }],
+        ['serif', { 'font-family': 'Shippori Mincho, serif' }],
+        ['mono', { 'font-family': 'Roboto Mono, monospace' }],
+    ]
 })
