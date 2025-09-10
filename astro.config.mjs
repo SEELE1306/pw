91f80a68fde596dcd0ddfa7ba0f747aeb168408f
SEELE1306 <mkdang@proton.me>
2025-09-10 23:27:23 +0200

    feat: add preliminary Prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro'
 
 export default defineConfig({
+    prefetch: true,
     site: "https://seele1306.com",
     integrations: [
         UnoCSS({
